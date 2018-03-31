7947a7228573eec3dc1f582a5a811fae78a27e1a
Patrick Miner <patrickminer318@gmail.com>
2018-03-31 12:08:41 -0700

    Added comments

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,10 @@
+// good BOILERPLATE for chrello
 var db = require('../db');
+// requires the DB connection
 
 module.exports = {
+  // exports an object with 2 key/value pairs, the values being objects themselves
+  // each interal object has 2 functions, a get function (to get all the info) and a post function (to post new messages/users)
   messages: {
     get: function () {}, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
